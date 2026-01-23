ba1991d23366eabba3f7ce7603af66dc2ce8bb0c
Aditya Solge <aditya.solge001@umb.edu>
2026-01-23 15:17:50 +0530

    Login/Register APIs

--- server.js
+++ server.js
@@ -15,11 +15,22 @@ app.post('/register', (request, response) => {
         });
     }
 
-    const newUser = { 
-        id: users.length + 1, 
-        name: name, 
-        email: email, 
-        password: password 
+    /**
+     * Implement logic to check if user with the same email
+     * already exist in the users object!
+     */
+    const user = users.find(u => u.email === email);
+    if (user) {
+        return response.status(400).json({
+            message: `User already exist with email: ${email}`
+        });
+    }
+
+    const newUser = {
+        id: users.length + 1,
+        name: name,
+        email: email,
+        password: password
     };
 
     users.push(newUser);
@@ -28,6 +39,34 @@ app.post('/register', (request, response) => {
         message: 'User registered',
         user: { id: newUser.id }
     });
+});
+
+/**
+ * Create a POST API with path /login which takes in email and password from
+ * body and checks if user with same email and password exist in the users
+ * array. If yes, return 200 response, otherwise 400 response.
+ */
+app.post('/login', (request, response) => {
+    const { email, password } = request.body;
+
+    if (!email || !password) {
+        return response.status(400).json({
+            message: 'Email and Password are required'
+        });
+    }
+
+    const user = users.find(u => u.email === email && u.password === password);
+
+    if (user) {
+        return response.status(200).json({
+            message: 'User authenticated',
+            user: user
+        });
+    } else {
+        return response.status(400).json({
+            message: 'Invalid email or password'
+        });
+    }
 });
 
 app.listen(5001, () => {
